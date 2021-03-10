d2286764907f7f4d6194749c93724b9e2b2f3e13
Miroslavetsh <dfgshte@gmail.com>
2021-03-10 21:43:30 +0200

    Breadcrumbs provided

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
+const MOBILE_WIDTH = 920
+
 const burger = document.querySelector('.header__burger')
 const navigation = document.querySelector('.nav')
 const page = document.querySelector('.page')
 const body = document.querySelector('body')
+const navSubItems = document.querySelectorAll('.nav__item--sub')
 
 try {
     const faqButtons = document.querySelectorAll('.faq__button')
 
-    faqButtons.forEach(button => {
+    faqButtons.forEach((button) => {
         button.addEventListener('click', function (event) {
             event.preventDefault()
 
@@ -32,6 +35,19 @@ function toggleBurger() {
     }
 }
 
+function toggleSubNav() {
+    if (window.innerWidth <= MOBILE_WIDTH) {
+        navSubItems.forEach((navItem) => {
+            navItem.addEventListener('click', function () {
+                this.classList.toggle('_active')
+            })
+        })
+    }
+}
+
+window.addEventListener('resize', toggleSubNav)
+window.addEventListener('load', toggleSubNav)
+
 // WEBP format 
 
 function testWebP(callback) {
@@ -41,4 +57,4 @@ function testWebP(callback) {
 		callback(webP.height == 2)
 	}
 	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-};
+}
